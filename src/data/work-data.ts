d791c07aa6b156b413de8c79bd3a15b664891eea
iracanyes <iracanyes@gmail.com>
2021-12-09 02:50:07 +0100

    éWork component

---
+++ src/data/work-data.ts
@@ -0,0 +1,283 @@
+export const workMobileData = [
+  {
+    icon: "/assets/img/netflix.png",
+    data: {
+      title: "Streaming app",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn streaming on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+  {
+    icon: "/assets/img/WhatsApp.svg.png",
+    data: {
+      title: "Chat app",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn social network on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+  {
+    icon: "/assets/img/uber.jpg",
+    data: {
+      title: "Geolocation app",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn streaming on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  }
+];
+
+export const workFrontendData = [
+  {
+    icon: "/assets/img/angular.jpg",
+    data: {
+      title: "This website",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn streaming on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+  {
+    icon: "/assets/img/React-1200px-icon.svg",
+    data: {
+      title: "Market platform",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn social network on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+  {
+    icon: "/assets/img/symfony-logo.svg",
+    data: {
+      title: "Server-side rendering",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn streaming on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  }
+];
+
+export const workBackendData = [
+  {
+    icon: "/assets/img/aws-logo.svg",
+    data: {
+      title: "Amazon Web services",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn streaming on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+  {
+    icon: "/assets/img/firebase-logo.png",
+    data: {
+      title: "Firebase services",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn social network on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+  {
+    icon: "/assets/img/symfony-logo.svg",
+    data: {
+      title: "PHP Back-end Framework",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn streaming on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+  {
+    icon: "/assets/img/raspberry-pi.svg",
+    data: {
+      title: "Microcomputer programming",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I've started learning the basic of computer science on some Raspberry Pi "
+    },
+  },
+  {
+    icon: "/assets/img/debian-logo.png",
+    data: {
+      title: "PHP Back-end framework",
+      images: [
+        {
+          src: "https://picsum.photos/400/400?random=1",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+        {
+          src: "https://picsum.photos/400/400?random=2",
+          title: "Netflix like homepage",
+          alt: "Netflix like homepage"
+        },
+
+      ],
+      content: "I wanted to learn streaming on mobile app as I consider it a must-know in the development of mobile application cross-platform for now and the future."
+    },
+  },
+];
